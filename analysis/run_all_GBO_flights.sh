1c7508390788c98c7c265f41b667e5822742dea1
newburgh <laura.newburgh@gmail.com>
2023-04-11 12:49:21 -0400

    included moving traceback plots into traceback_plots dir

--- analysis/run_all_GBO_flights.sh
+++ analysis/run_all_GBO_flights.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for i in {8..12} 
+for i in {0..12} 
 do
     python run_one_flight.py $i
 done
+
+cd /hirax/GBO_Analysis_Outputs
+mv /hirax/GBO_Analysis_Outputs/FLY* /hirax/GBO_Analysis_Outputs/main_beam_fits
+
